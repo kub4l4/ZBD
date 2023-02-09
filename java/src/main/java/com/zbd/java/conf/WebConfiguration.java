6b59bf52a820013bf62204910f118d78bc4ffa5a
Kamil <kamil.kubala@premotec.ch>
2023-02-09 17:43:20 +0100

    docker fix, cleaning

--- java/src/main/java/com/zbd/java/conf/WebConfiguration.java
+++ java/src/main/java/com/zbd/java/conf/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.zbd.java;
+package com.zbd.java.conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
